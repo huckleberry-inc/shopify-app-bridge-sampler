89711e773e0feb4b77cb09188367ec6413fdcf24
camelmasa <camelmasa@gmail.com>
2020-03-17 00:31:06 +0900

    feat: first features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,170 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ClientApplication } from "@shopify/app-bridge";
+import { Page, Card, Button } from "@shopify/polaris";
 
-function App() {
+import {
+  TitleBar,
+  ContextualSaveBar,
+  ResourcePicker,
+  Loading,
+  Modal,
+  Toast
+} from "@shopify/app-bridge/actions";
+
+export const App: React.FC<{ app: ClientApplication<unknown> }> = ({ app }) => {
+  // TitleBar
+  const updateTitleBar = () => {
+     const titleBarOptions = {
+       title: "App Bridge Sampler"
+     };
+    TitleBar.create(app, titleBarOptions);
+  };
+
+  // ContextualSaveBar
+  const options = {
+    saveAction: {
+      disabled: false,
+      loading: false
+    },
+    discardAction: {
+      disabled: false,
+      loading: false
+    }
+  };
+  const contextualSaveBar = ContextualSaveBar.create(app, options);
+
+  const showContextualSaveBar = () => {
+    contextualSaveBar.dispatch(ContextualSaveBar.Action.SHOW);
+  };
+
+  contextualSaveBar.subscribe(ContextualSaveBar.Action.DISCARD, () => {
+    console.log("Clicked discard button");
+    contextualSaveBar.dispatch(ContextualSaveBar.Action.HIDE);
+  });
+
+  contextualSaveBar.subscribe(ContextualSaveBar.Action.SAVE, () => {
+    console.log("Clicked save button");
+    contextualSaveBar.dispatch(ContextualSaveBar.Action.HIDE);
+  });
+
+  // ResourcePicker
+  const productPicker = ResourcePicker.create(app, {
+    resourceType: ResourcePicker.ResourceType.Product
+  });
+  const openProductPicker = () => {
+    productPicker.dispatch(ResourcePicker.Action.OPEN);
+  };
+
+  const productVariantPicker = ResourcePicker.create(app, {
+    resourceType: ResourcePicker.ResourceType.ProductVariant
+  });
+  const openProductVariantPicker = () => {
+    productVariantPicker.dispatch(ResourcePicker.Action.OPEN);
+  };
+
+  const collectionPicker = ResourcePicker.create(app, {
+    resourceType: ResourcePicker.ResourceType.Collection
+  });
+  const openCollectionPicker = () => {
+    collectionPicker.dispatch(ResourcePicker.Action.OPEN);
+  };
+
+  // Features
+  const showFeatures = () => {
+    app.featuresAvailable().then(state => {
+      console.log(state);
+    });
+  };
+
+  // Loading
+  const loading = Loading.create(app);
+
+  const startLoading = () => {
+    loading.dispatch(Loading.Action.START);
+  };
+
+  const stopLoading = () => {
+    loading.dispatch(Loading.Action.STOP);
+  };
+
+  // Modal
+  const modalOptions = {
+    title: 'Title',
+    message: "Message",
+  };
+  const modal = Modal.create(app, modalOptions);
+  const openModal = () => {
+    modal.dispatch(Modal.Action.OPEN);
+  };
+  modal.subscribe(Modal.Action.CLOSE, () => {
+    console.log("Closed modal");
+  });
+
+  // Toast
+  const toastOptions = {
+    message: "Message",
+    duration: 5000
+  };
+  const toast = Toast.create(app, toastOptions);
+  const showToast = () => {
+    toast.dispatch(Toast.Action.SHOW);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Page narrowWidth title="App Bridge Sampler">
+      <Card title="TitleBar" sectioned>
+        <Button onClick={updateTitleBar} primary>
+          Update
+        </Button>
+      </Card>
 
-export default App;
+      <Card title="ContextualSaveBar" sectioned>
+        <Button onClick={showContextualSaveBar} primary>
+          Show
+        </Button>
+      </Card>
+
+      <Card title="ResourcePicker" sectioned>
+        <Button onClick={openProductPicker} primary>
+          Open (for product)
+        </Button>
+
+        <Button onClick={openProductVariantPicker} primary>
+          Open (for variant)
+        </Button>
+ 
+        <Button onClick={openCollectionPicker} primary>
+          Open (for collection)
+        </Button>
+      </Card>
+
+      <Card title="Features" sectioned>
+        <Button onClick={showFeatures} primary>
+          Show (Just console)
+        </Button>
+      </Card>
+
+      <Card title="Loading" sectioned>
+        <Button onClick={startLoading} primary>
+          Start
+        </Button>
+
+        <Button onClick={stopLoading} primary>
+          Stop
+        </Button>
+      </Card>
+
+      <Card title="Modal" sectioned>
+        <Button onClick={openModal} primary>
+          Open
+        </Button>
+      </Card>
+
+      <Card title="Toast" sectioned>
+        <Button onClick={showToast} primary>
+          Show
+        </Button>
+      </Card>
+    </Page>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as queryString from "query-string";
+import translations from "@shopify/polaris/locales/en.json";
+import { Provider } from "@shopify/app-bridge-react";
+import createApp from "@shopify/app-bridge";
+import { AppProvider } from "@shopify/polaris";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "@shopify/polaris/styles.css";
+
+const parsed = queryString.parse(window.location.search);
+const shopOrigin = parsed.shop as string;
+
+// Assign constants
+const SHOPIFY_API_KEY = "8c4f9df76d2c788625df3830b69378e0";
+
+// Install app flow
+const setup = () => {
+  if (window.top === window.self) {
+    const redirectUri = `https://${shopOrigin}/admin/apps/app-bridge-sampler`;
+    const permissionUrl = `/oauth/authorize?client_id=${SHOPIFY_API_KEY}&scope=write_script_tags&redirect_uri=${redirectUri}`;
+
+    window.location.assign(`https://${shopOrigin}/admin${permissionUrl}`);
+
+    return;
+  }
+
+  const config = { apiKey: SHOPIFY_API_KEY, shopOrigin };
+  const app = createApp(config);
+
+  ReactDOM.render(
+    <AppProvider i18n={translations}>
+      <Provider config={config}>
+        <App app={app} />
+      </Provider>
+    </AppProvider>,
+    document.getElementById("root")
+  );
+};
+setup();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
